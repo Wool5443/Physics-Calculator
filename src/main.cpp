5ef307a221cb961f869960ce44100edbe535afd0
Wool5443 <mihsolodilov2015@gmail.com>
2024-06-17 22:44:16 +0300

    Added interactive mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,30 @@
 
 static const char* LOG_FOLDER = "../log";
 
-int main()
+int main(int argc, char* argv[])
 {
-    PRINT_ERROR(Tree::StartLogging(LOG_FOLDER));
-
     String expression = {};
-    expression.Create(32);
-
-    // strcpy(expression.buf, "1 + 2 + 3 + 4");
-    strcpy(expression.buf, "(1 + 2 + 3 + 4) * 2 ^ 2");
+    expression.Create(256);
+    
+    scanf("%s", expression.buf);
 
     expression.Filter();
+
+    #ifndef NDEBUG
+    PRINT_ERROR(Tree::StartLogging(LOG_FOLDER));
+    #endif
 
     Tree tree = {};
 
     ErrorCode err = ParseExpression(&tree, &expression);
 
+    #ifndef NDEBUG
     tree.Dump();
-
     Tree::EndLogging();
+    #endif
 
     tree.Destructor();
+    expression.Destructor();
 
     return 0;
 }
